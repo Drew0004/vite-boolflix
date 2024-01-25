511ecb400d165630e2b286da6ebf65411e85fb48
Drew0004 <drdrewgraphic@gmail.com>
2024-01-25 10:12:12 +0100

    Add trend api

--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    trendUrl: 'https://api.themoviedb.org/3/trending/all/day?api_key=dfa13031b80dd317ebfeae295f5282f6',
+    
+    trendUrlMovies: 'https://api.themoviedb.org/3/trending/movie/day?api_key=dfa13031b80dd317ebfeae295f5282f6',
+    trendUrlSeries: 'https://api.themoviedb.org/3/trending/tv/day?api_key=dfa13031b80dd317ebfeae295f5282f6',
+    
     baseUrlMovies: 'https://api.themoviedb.org/3/search/movie?api_key=dfa13031b80dd317ebfeae295f5282f6&query=',
     baseUrlSeries: 'https://api.themoviedb.org/3/search/tv?api_key=dfa13031b80dd317ebfeae295f5282f6&query=',
+    
     baseUrlFlags: 'https://flagsapi.com/IT/flat/64.png',
     baseUrlImg:'https://image.tmdb.org/t/p/w780/',
+    
     searchName: '',
     movies : [],
     series : [], 
